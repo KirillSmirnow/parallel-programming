98d553f5330f4882c266fa9654905138e7ec32a4
Kirill Smirnov <Smirnov.K@numdes.com>
2020-02-28 14:37:02 +0300

    Build program structure

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,40 @@
 #include "Matrix.cpp"
+#include "Multiplication.cpp"
 
 using namespace std;
 
-int main() {
-    srandom(5);
+void generateMatrices(int height, int width) {
     generateAndPersistMatrices(5);
+}
+
+void executeMultiplication(int mode) {
     Matrix *A, *B;
     fetchMatrices(&A, &B);
-    A->writeToStdout();
-    B->writeToStdout();
+
+    Multiplication *multiplication;
+    switch (mode) {
+    }
+
+    multiplication->execute();
+}
+
+int main(int argc, char **argv) {
+    srandom(5);
+    if (argc < 2) {
+        return 1;
+    }
+    if (atoi(argv[1]) == 1) {
+        if (argc != 4) {
+            return 1;
+        }
+        generateMatrices(atoi(argv[2]), atoi(argv[3]));
+    } else if (atoi(argv[1]) == 2) {
+        if (argc != 3) {
+            return 1;
+        }
+        executeMultiplication(atoi(argv[2]));
+    } else {
+        return 1;
+    }
     return 0;
 }
