fad1c53a17582ef7adfd182893ce728a2bba1bb0
Kirill Smirnov <Smirnov.K@numdes.com>
2020-02-28 17:52:52 +0300

    Implement banchmarks output

--- py/main.py
+++ py/main.py
@@ -1,6 +1,6 @@
 import time
 import subprocess
-import matplotlib
+import matplotlib.pyplot as plot
 
 EXECUTABLE = "../cpp/cmake-build-debug/open_mp"
 
@@ -20,6 +20,11 @@ matrix_sizes = (
     (1000, 1000),
 )
 
+benchmarks = {
+    mode: {size: -1 for size in matrix_sizes}
+    for mode in calculation_modes
+}
+
 
 def benchmark():
     for matrix_size in matrix_sizes:
@@ -28,12 +33,14 @@ def benchmark():
 
 def benchmark_matrix(height, width):
     print("Benchmarking matrix of size %d×%d" % (height, width))
+    size = height * width
     subprocess.call([EXECUTABLE, "1", str(height), str(width)])
     for mode in calculation_modes:
-        benchmark_matrix_mode(mode)
+        duration = benchmark_matrix_mode(mode)
+        benchmarks[mode][size] = duration
 
 
-def benchmark_matrix_mode(mode):
+def benchmark_matrix_mode(mode) -> float:
     print("Benchmarking mode %d" % mode)
     iterations = 100
     start = time.time()
@@ -42,7 +49,9 @@ def benchmark_matrix_mode(mode):
     finish = time.time()
     duration = (finish - start) / 100
     print("Duration: %.2f seconds" % duration)
+    return duration
 
 
 if __name__ == '__main__':
     benchmark()
+    print("Results", benchmarks)
