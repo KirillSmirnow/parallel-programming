f09b969f3bf080847e5ce99cc4b6d38b081b72ee
Kirill Smirnov <smirnowku@gmail.com>
2020-03-26 17:24:45 +0300

    Add quicksort-test

---
+++ quicksort/quicksort-test.py
@@ -0,0 +1,54 @@
+import time
+import subprocess
+import matplotlib.pyplot as plot
+
+EXECUTABLE = "./quicksort-impl/cmake-build-release/quicksort"
+
+processes_counts = (1, 2, 4)
+array_lengths = (10_000, 100_000, 1_000_000)
+
+benchmarks = {
+    processes_count: {length: -1 for length in array_lengths}
+    for processes_count in processes_counts
+}
+
+
+def benchmark():
+    for array_length in array_lengths:
+        benchmark_array(array_length)
+
+
+def benchmark_array(array_length):
+    print(f"Benchmarking array of length {array_length}")
+    for processes_count in processes_counts:
+        duration = benchmark_array_with_processes_count(processes_count)
+        benchmarks[processes_count][array_length] = duration
+
+
+def benchmark_array_with_processes_count(processes_count) -> float:
+    print(f"Benchmarking {processes_count} processes")
+    iterations = 100
+    start = time.time()
+    for i in range(iterations):
+        exit_code = subprocess.run(["mpiexec", "-n", str(processes_count), EXECUTABLE])
+        assert exit_code == 0
+    finish = time.time()
+    duration = (finish - start) / iterations
+    print("Duration: %.2f seconds" % duration)
+    return duration
+
+
+def plot_results():
+    serial_durations = tuple(benchmarks[0].values())
+    for processes_count in benchmarks:
+        durations = tuple(benchmarks[processes_count].values())
+        accelerations = tuple(sd / d for sd, d in zip(serial_durations, durations))
+        plot.plot(accelerations, marker='o')
+    plot.legend(processes_counts)
+    plot.show()
+
+
+if __name__ == '__main__':
+    benchmark()
+    print("Results", benchmarks)
+    plot_results()
