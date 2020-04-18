2f6f21469a251dd0e2c63c788632e8b187e5cbd2
Kirill Smirnov <smirnowku@gmail.com>
2020-04-18 20:44:56 +0300

    Implement precisionReached() Implement dispatching bounds of array x among processes

--- linear-system/linear-system-impl/LinearSystemSolver.cpp
+++ linear-system/linear-system-impl/LinearSystemSolver.cpp
@@ -11,11 +11,16 @@ private:
     int currentProcess;
     int totalProcesses;
 
+    // this process will calculate a part [offset; offset + size] of array x
+    int offset;
+    int size;
+
     int systemSize;
     double precision;
     double *A;        // systemSize x systemSize; A[i][j] = i * systemSize + j
     double *b;        // systemSize
     double *x;        // systemSize
+    double *xPrevious = nullptr;   // systemSize
 
 public:
     LinearSystemSolver() {
@@ -40,13 +45,40 @@ public:
         MPI_Bcast(A, systemSize * systemSize, MPI_DOUBLE, PRIMARY_PROCESS, MPI_COMM_WORLD);
         MPI_Bcast(b, systemSize, MPI_DOUBLE, PRIMARY_PROCESS, MPI_COMM_WORLD);
         MPI_Bcast(x, systemSize, MPI_DOUBLE, PRIMARY_PROCESS, MPI_COMM_WORLD);
+
+        if (currentProcess == PRIMARY_PROCESS) {
+            const int partSize = systemSize / totalProcesses;
+            for (int process = 0; process < totalProcesses; process++) {
+                int offset = process * partSize;
+                int size = (process < totalProcesses - 1) ? partSize : systemSize - offset;
+                if (process == PRIMARY_PROCESS) {
+                    this->offset = offset;
+                    this->size = size;
+                } else {
+                    MPI_Send(&offset, 1, MPI_INT, process, 0, MPI_COMM_WORLD);
+                    MPI_Send(&size, 1, MPI_INT, process, 0, MPI_COMM_WORLD);
+                }
+            }
+        } else {
+            MPI_Status status;
+            MPI_Recv(&offset, 1, MPI_INT, PRIMARY_PROCESS, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(&size, 1, MPI_INT, PRIMARY_PROCESS, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        }
     }
 
     void computeIteration() {
     }
 
     bool precisionReached() {
-        return false;
+        if (xPrevious == nullptr) {
+            return false;
+        }
+        for (int i = 0; i < systemSize; i++) {
+            if (abs(x[i] - xPrevious[i]) > precision) {
+                return false;
+            }
+        }
+        return true;
     }
 
     void outputResult() {
