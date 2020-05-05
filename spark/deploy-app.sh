d3434de36b3ad314841d9417cf15dd8b45b68606
Kirill Smirnov <smirnowku@gmail.com>
2020-05-05 19:18:58 +0300

    Create deploy-app script

---
+++ spark/deploy-app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build app
+cd nasa-logs
+mvn clean package
+cd ..
+
+# Transfer app's jar into container
+cp ./nasa-logs/target/*jar-with-dependencies.jar app.jar
+docker cp app.jar "spark_master:/opt/bitnami/spark"
+rm -f app.jar
+
+# Submit app
+docker exec "spark_master" ./bin/spark-submit app.jar
